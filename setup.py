888d6ac5ab73a8f589d04bd081ecdf37ab6e0cef
Thomas Grainger <tagrain@gmail.com>
2019-01-29 15:12:58 +0000

    rename to pywinrm2

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from os import path
 
 from setuptools import setup
 
-__version__ = '0.3.1.dev0'
-project_name = 'pywinrm'
+__version__ = '0.0.0'
+project_name = 'pywinrm2'
 
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
